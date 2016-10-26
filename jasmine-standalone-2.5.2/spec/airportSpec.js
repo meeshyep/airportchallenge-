46894a8280cfd1acbedaf2bb251a14e08234f617
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-26 18:02:26 +0100

    Added changing capacity and prevent landing when full

--- jasmine-standalone-2.5.2/spec/airportSpec.js
+++ jasmine-standalone-2.5.2/spec/airportSpec.js
@@ -14,23 +14,38 @@ describe('airport ',function(){
   });
 
   it('can land a plane', function(){
-    airport.land(plane)
+    airport.land(plane);
     expect(airport.planes).toContain(plane);
   });
 
   it('can instruct a plane to take off', function(){
     spyOn(airport.weather, 'isStormy').and.returnValue(false);
-    airport.land(plane)
-    airport.takeoff(plane)
+    airport.land(plane);
+    airport.takeoff(plane);
     expect(airport.planes).toEqual([]);
   });
 
   it('prevents takeoff when weather is stormy', function() {
     spyOn(airport.weather, 'isStormy').and.returnValue(true);
-    airport.land(plane)
+    airport.land(plane);
     expect(function(){
-      airport.takeoff(plane)
+      airport.takeoff(plane);
     }).toThrowError("Can't take off: stormy");
   });
+
+  it('prevent landing when the airport is full', function(){
+    airport.land(plane);
+    airport.land(plane);
+    airport.land(plane);
+    expect(function(){
+      airport.land(plane);
+    }).toThrowError("Unable to land, airport full");
+  });
+
+  it('allow default capacity to be changed as required', function(){
+    airport.capacity = 5;
+    expect(airport.capacity).toEqual(5);
+  });
+
 
 });
--- jasmine-standalone-2.5.2/src/airport.js
+++ jasmine-standalone-2.5.2/src/airport.js
@@ -1,34 +1,37 @@
 function Airport() {
   this.planes = [];
   this.weather = new Weather();
-};
+  this.capacity = 3;
+}
 
 Airport.prototype.land = function(plane) {
+  if(this.planes.length >= this.capacity){
+    throw new Error("Unable to land, airport full");
+  }
   this.planes.push(plane);
-
 };
 
 Airport.prototype.takeoff = function(plane) {
-  if(this.weather.isStormy()){throw new Error("Can't take off: stormy")};
+  if(this.weather.isStormy()){throw new Error("Can't take off: stormy");}
   var index = this.planes.indexOf(plane);
   if (index > -1) {
-    this.planes.splice(index, 1)
-  };
+    this.planes.splice(index, 1);
+  }
 };
+
+
 
 function Plane() {
 
-};
-
-
+}
 
 function Weather() {
   this.isStormy = function() {
     if(Math.random() > 0.9){
       return true;
-    };
+    }
   };
-};
+}
 
 
 
